e1dab6f8fb7e47a7536a4d359d0ea180eb350c6d
blekmus <dinil@protonmail.com>
2022-10-04 15:20:56 +0530

    python requirements removed versions

--- cli.js
+++ cli.js
@@ -120,7 +120,7 @@ async function main() {
     }
   }
 
-  // run shots n shots formatter
+  // run shots n shorts formatter
   if (args.flags.format) {
     await structureFormatter(config.get('location'))
     return
